3b2d9f44e56ef34627b77250413fd263980658ed
Jonathan Gomez <107599365+JonnycatMeow@users.noreply.github.com>
2024-03-09 17:36:15 -0800

    Add files via upload

---
+++ 0.6.3/20151110_VHS/20151110_VHS.lua
@@ -0,0 +1,54 @@
+local shaderName = "20151110_VHS"
+function onCreate()
+    shaderCoordFix() -- initialize a fix for textureCoord when resizing game window
+
+    makeLuaSprite("20151110_VHS")
+    makeGraphic("shaderImage", screenWidth, screenHeight)
+
+   setSpriteShader("shaderImage", "20151110_VHS")
+
+
+    runHaxeCode([[
+        var shaderName = "]] .. shaderName .. [[";
+        
+        game.initLuaShader(shaderName);
+        
+        var shader0 = game.createRuntimeShader(shaderName);
+        game.camGame.setFilters([new ShaderFilter(shader0)]);
+        game.getLuaObject("20151110_VHS").shader = shader0; // setting it into temporary sprite so luas can set its shader uniforms/properties
+        return;
+    ]])
+end
+
+function onUpdate(elapsed)
+    setShaderFloat("20151110_VHS", "iTime", os.clock())
+ end
+
+function shaderCoordFix()
+    runHaxeCode([[
+        resetCamCache = function(?spr) {
+            if (spr == null || spr.filters == null) return;
+            spr.__cacheBitmap = null;
+            spr.__cacheBitmapData = null;
+        }
+        
+        fixShaderCoordFix = function(?_) {
+            resetCamCache(game.camGame.flashSprite);
+            resetCamCache(game.camHUD.flashSprite);
+            resetCamCache(game.camOther.flashSprite);
+        }
+    
+        FlxG.signals.gameResized.add(fixShaderCoordFix);
+        fixShaderCoordFix();
+        return;
+    ]])
+    
+    local temp = onDestroy
+    function onDestroy()
+        runHaxeCode([[
+            FlxG.signals.gameResized.remove(fixShaderCoordFix);
+            return;
+        ]])
+        if (temp) then temp() end
+    end
+end
